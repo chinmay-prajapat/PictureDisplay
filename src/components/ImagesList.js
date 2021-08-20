87c83f9911e9a5cf3f49857c932c5a21a723c75d
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-08-20 05:36:04 +0530

    Pics app

--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,12 +1,15 @@
 import React from "react";
-const ImageList = (props) => {
-  return props.images.map(({ description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
-  });
-  //   const img = props.images.map(({ description, id, urls }) => {
-  //     return <img key={id} src={urls.regular} alt={description} />;
-  //   });
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 
-  //   return <div className="ui container">{img}</div>;
+const ImageList = (props) => {
+  // return props.images.map(({ description, id, urls }) => {
+  //   return <img key={id} src={urls.regular} alt={description} />;
+  // });
+  const img = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
+  });
+
+  return <div className="image-list">{img}</div>;
 };
 export default ImageList;
